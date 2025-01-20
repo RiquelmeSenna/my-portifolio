0d43ae9a736b9c12f178f072498b3b82bbb6ba0c
Riquelme <riquelmestayler@gmail.com>
2025-01-20 12:40:09 -0300

    done project my-portifolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,16 @@ let buttons = document.querySelectorAll('.buttons a li');
 let title = document.querySelectorAll('.title');
 let main = document.querySelector('main');
 let technology = document.querySelectorAll('.technology');
+let menuBar = document.querySelector('.bar-menu')
+let buttonAside = document.querySelector('aside button')
 
 aside.style.scrollbarWidth = 'none';
 
 buttons.forEach(item => {
     item.addEventListener('click', (e) => {
+        if (window.innerWidth <= 768) {
+            aside.style.marginLeft = '-100%';
+        }
         e.preventDefault();
         item.classList.add('active');
         if (item.classList.contains('active')) {
@@ -54,4 +59,12 @@ technology.forEach(item => {
             svg.style.stroke = 'white';
         });
     });
+});
+
+menuBar.addEventListener('click', () => {
+    aside.style.marginLeft = '0';
+})
+
+buttonAside.addEventListener('click', () => {
+    aside.style.marginLeft = '-100%';
 });
